1442c56cac02381af3c8bc60271c131ee9305bbc
fanjiapeng <925158289@qq.com>
2023-07-21 15:06:03 +0800

    cesium init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  position: absolute;
+  z-index: 999;
+  line-height: 1.5;
+  max-height: 100vh;
+  color: #fff;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {Viewer, Ion} from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import { onMounted } from "vue";
+
+
+onMounted(() => {
+  console.log(document.getElementById('cesiumContainer'))
+  Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwODQzYmNiNS1mZjM5LTRmN2UtYmFhNy05ZTdlMWQ3NDg0YTYiLCJpZCI6MTU1NDg2LCJpYXQiOjE2ODk5MDkwNTR9.6yDZWcBPP7Zb6-wU-aMXGTqcuA6yzqg4WEwFc8QD7qY'
+  const viewer = new Viewer("cesiumContainer");
+})
+</script>
+
+<template>
+  <main>
+    <div class="cesiumContainer" id="cesiumContainer"></div>
+  </main>
+</template>
+<style scoped>
+.cesiumContainer {
+  width: 100%;
+  height: 100vh;
+}
+</style>
