0f006a8711b51293b7920db6e6a5805860a1165c
Mawawa <xiaoruiaini88@163.com>
2023-07-23 17:20:35 +0800

    init cesium world

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">World</RouterLink>
+        <!-- <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
---
+++ src/views/World.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Viewer } from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log(document.getElementById('cesiumContainer'))
+  const viewer = new Viewer('cesiumContainer')
+  viewer.scene.debugShowFramesPerSecond = true
+})
+</script>
+
+<template>
+  <main>
+    <div class="cesiumContainer" id="cesiumContainer"></div>
+  </main>
+</template>
+<style scoped>
+.cesiumContainer {
+  width: 100%;
+  height: 100vh;
+}
+</style>
