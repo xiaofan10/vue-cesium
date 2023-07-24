a310b3e2aa51c0cb7302d3581bd312c26503be4b
Mawawa <xiaoruiaini88@163.com>
2023-07-24 22:51:00 +0800

    init:初始化官方资源，设置基础图层

--- README.md
+++ README.md
@@ -9,27 +9,16 @@ This template should help get you started developing with Vue 3 in Vite.
 ## Customize configuration
 
 See [Vite Configuration Reference](https://vitejs.dev/config/).
+## cesium 使用手册
 
-## Project Setup
+### [中文文档](https://www.wenjiangs.com/doc/cesium-api-mapboxstyleimageryprovider)
+- 高德地图 https://blog.csdn.net/hou_ge/article/details/127212932
+- 矢量地图带注记：https://webrd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}  UrlTemplateImageryProvider
+- 矢量地图不带注记：http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}&scl=1&ltype=3
+- 影像不带注记：http://webst0{1-4}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}
+- 道路带注记：http://webst0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8
+- 道路不带注记：http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=8<ype=11
+- 矢量带注记http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}
 
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+- 天地图 http://lbs.tianditu.gov.cn/server/MapService.html
+- http://Mapbox.com 地图制作 https://blog.csdn.net/hou_ge/article/details/127212932
--- src/views/World.vue
+++ src/views/World.vue
@@ -1,12 +1,47 @@
 <script setup>
-import { Viewer } from 'cesium'
+import * as Cesium from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 import { onMounted } from 'vue'
 
-onMounted(() => {
-  console.log(document.getElementById('cesiumContainer'))
-  const viewer = new Viewer('cesiumContainer')
-  viewer.scene.debugShowFramesPerSecond = true
+onMounted(async () => {
+  // 官方提供 https://services.arcgisonline.com/arcgis/rest/services
+  // 高德地图 https://blog.csdn.net/hou_ge/article/details/127212932
+  // 矢量地图带注记：https://webrd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}  UrlTemplateImageryProvider
+  // 矢量地图不带注记：http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}&scl=1&ltype=3
+  // 影像不带注记：http://webst0{1-4}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}
+  // 道路带注记：http://webst0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8
+  // 道路不带注记：http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=8<ype=11
+  // 矢量带注记http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}
+
+  // 天地图 http://lbs.tianditu.gov.cn/server/MapService.html
+  // http://Mapbox.com 地图制作 https://blog.csdn.net/hou_ge/article/details/127212932
+
+  // const viewer = new Viewer('cesiumContainer')
+  // let tdtLayer = new UrlTemplateImageryProvider({
+  // 		url: "https://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer",
+  // 		minimumLevel: 3,
+  // 		maximumLevel: 18
+  // 	})
+  // 	viewer.imageryLayers.addImageryProvider(tdtLayer);
+
+  //  ArcGis 地图加载方法
+  // Cesium.ArcGisMapService.defaultAccessToken =
+  //   'AAPKa334085b43db448b9396da0be43d8eb0E00TOp76Ro_zfiObeIZ-Bkhu1q5J1GEASdk78VTLE52iPRJJa6E_O4O84zOVYEp1'
+  // Cesium 官方提供的资源touken
+  // const customMap = Cesium.ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/arcgis/rest/services/World_Terrain_Base/MapServer')
+  // let imageryProviderConfig = new Cesium.SingleTileImageryProvider({
+  //     url: 'https://mapv-data.oss-cn-hangzhou.aliyuncs.com/Cesium-1.82-hawk/background.png',
+  //   })
+  
+  // Cesium 官方提供的资源所使用的token
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkY2UyZDI3NS02M2NjLTQ1OTUtODBmMC03YWNhYzk1NzU2M2MiLCJpZCI6MTU2MDQyLCJpYXQiOjE2OTAyMDc4ODR9.BmN3pOpnSPgJa2eBzYBHt5xrnMaIlV7MdcNrvowXpfs'
+  // ArcGis 使用的token https://developers.arcgis.com
+  // Cesium.ArcGisMapService.defaultAccessToken = 'AAPKa334085b43db448b9396da0be43d8eb0E00TOp76Ro_zfiObeIZ-Bkhu1q5J1GEASdk78VTLE52iPRJJa6E_O4O84zOVYEp1'
+  const viewer = new Cesium.Viewer('cesiumContainer', {
+    baseLayerPicker: false,
+    baseLayer: Cesium.ImageryLayer.fromProviderAsync(Cesium.IonImageryProvider.fromAssetId(4))
+  })
+  
 })
 </script>
 
